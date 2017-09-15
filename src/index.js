86dc6f1fb7b1412b860d0e1bd79f45e282241e12
Gabriel Paradiso <gabriel.paradiso@typeform.com>
2017-09-15 22:07:51 +0200

    display video details and being able to play video

--- src/index.js
+++ src/index.js
@@ -1,15 +1,31 @@
-import React from 'react';
-import ReactDOM from 'react-dom'
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
+import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
 
-import SearchBar from './components/search_bar'
+const API_KEY = 'AIzaSyCAbqvTaafcDNUn4Hsox0W5TOCP1T3v9Hc';
 
-const API_KEY = 'TnSUMDMAEpV-9tYzt5opzAfH';
+class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { videos: [] };
 
-const App = () => {
-    return (
-    <div>
-        <SearchBar />
-    </div>);
+        YTSearch({key: API_KEY, term: 'wachiturros'}, (videos) => {
+            this.setState({ videos })
+        });
+
+    }
+    render(){
+        return (
+            <div>
+                <SearchBar />
+                <VideoDetail video={this.state.videos[0]}/>
+                <VideoList videos={this.state.videos}/>
+            </div>
+        );
+    }
 }
 
 ReactDOM.render(<App />, document.querySelector('.container'));
